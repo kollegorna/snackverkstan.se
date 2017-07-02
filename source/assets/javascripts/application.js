c0e3681b7c6d0fb93fe80ae8906d9e48a760c124
Per Sandström <per@helloper.com>
2017-07-02 20:35:40 +0200

    Basic markup

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,16 @@
-// …
+//=require jquery
+//=require formchimp
+
+(function($) {
+  $(".newsletter").formchimp({
+    'errorMessage': 'Aj… Något gick fel i kontakten med databasen. Försök prenumerera igen.',
+    'responseClass': 'newsletter__response',
+    'successMessage': 'Kul att du är intresserad! Du får strax ett mail för att bekräfta din prenumeration.',
+    'onMailChimpSuccess': function() {
+
+    },
+    'onMailChimpError': function() {
+
+    }
+  });
+})(jQuery);
